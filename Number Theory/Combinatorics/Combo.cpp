4b203c84e3c49592c39549c27bbccff7bc6755f3
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-10-02 14:57:59 -0500

    Update Combo.cpp

--- Number Theory/Combinatorics/Combo.cpp
+++ Number Theory/Combinatorics/Combo.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 #define ll long long
 
-ll nCk(int n, int k){
-    
+ll nCk(ll n, ll k){
+    ll comp = n-k;
+    ll ans = 1;
+    for(ll i = n; i>max(k, comp); i--){
+        ans*=n;
+    }
+    for(int i = 2; i<=min(k, comp); i++){
+        ans/=i;
+    }
+    return ans;
 }
-ll nPk(int n, int k){
-    
+ll nPk(ll n, ll k){
+    ll comp = n-k;
+    ll ans = 1;
+    for(ll i = n; i>max(k, comp); i--){
+        ans*=n;
+    }
 }
